8d5f68d6ad6c5f1d0f8da81080b07187cc91a763
Legnakra <mariajesus.alloza@outlook.es>
2022-12-17 19:09:55 +0100

    COmprobar Pago Finalizado

--- Procedimientos.sql
+++ Procedimientos.sql
@@ -27,14 +27,16 @@ EXEC ClienteInexistente ('54890865P');
 
 
 ---Procedimiento que, ingresando el código de la actividad comprueba si existe en la tabla actividades.
-CREATE OR REPLACE PROCEDURE ActividadInexistente (v_codactividad actividades.codigo%type) IS
-    v_actividad NUMBER;
+CREATE OR REPLACE PROCEDURE ActividadInexistente (v_codactividad actividades.codigo%type)
+IS
+    CURSOR c_codigo IS
+        SELECT *
+        FROM actividades
+        WHERE codigo=v_codactividad;
+    v_codigo actividades.codigo%type;
 BEGIN
-    SELECT COUNT(*) INTO v_actividad
-    FROM actividades
-    WHERE codigo=v_codactividad;
-    IF v_actividad=0 THEN
-        RAISE_APPLICATION_ERROR(-20001,'La actividad especificada no existe');
+    IF v_codigo IS NULL THEN
+        RAISE_APPLICATION_ERROR(-20002,'La actividad especificada no existe');
     END IF;
 END;
 /
@@ -54,7 +56,7 @@ BEGIN
     FROM actividadesrealizadas
     WHERE codigoactividad=v_codactividad AND codigoestancia=(SELECT COUNT(codigo) FROM estancias WHERE codigoregimen = 'TI');
     IF v_todoIncluido>0 THEN
-        RAISE_APPLICATION_ERROR(-20001,'La actividad especificada se ha realizado en regimen de Todo Incluido');
+        RAISE_APPLICATION_ERROR(-20003,'La actividad especificada se ha realizado en regimen de Todo Incluido');
     END IF;
 END;
 /
@@ -73,33 +75,44 @@ BEGIN
     SELECT codigo INTO v_cliente
     FROM estancias WHERE nifcliente=v_codcliente AND codigo IN (SELECT codigoestancia FROM actividadesrealizadas WHERE codigoactividad=v_codactividad);
     IF v_cliente IS NULL THEN
-        RAISE_APPLICATION_ERROR(-20001,'El cliente nunca ha realizado esa actividad');
+        RAISE_APPLICATION_ERROR(-20004,'El cliente nunca ha realizado esa actividad');
     END IF;
 END;
 /
 
+---Funciona correctamente
 EXEC ClienteRealizaActividad ('69191424H', 'B302');
 
 
 ---Procedimiento que compruebe si el cliente ha pagado la última actividad con ese código que ha realizado introduciendo el código de la actividad y el NIF del cliente.
-CREATE OR REPLACE PROCEDURE ActividadAbonada (v_codcliente personas.nif%type, v_codactividad actividades.codigo%type) IS
-    CURSOR c_abonado IS
+CREATE OR REPLACE PROCEDURE ActividadAbonada (v_codcliente personas.nif%type, v_codactividad actividades.codigo%type)
+IS
+    CURSOR c_actividad_abonada IS
         SELECT *
         FROM actividadesrealizadas
         WHERE codigoestancia = (SELECT codigo FROM estancias WHERE nifcliente=v_codcliente) AND codigoactividad=v_codactividad
         ORDER BY fecha DESC
         FETCH FIRST 1 ROWS ONLY;
-    v_abonada actividadesrealizadas.abonado%type;
+    v_actividad_abonada actividadesrealizadas%ROWTYPE;
 BEGIN
-    IF v_abonada='S'
-    THEN
-        DBMS_OUTPUT.PUT_LINE('TRUE');
-    ELSE
+    OPEN c_actividad_abonada;
+    FETCH c_actividad_abonada INTO v_actividad_abonada;
+    IF v_actividad_abonada.abonado = 'N' THEN
         DBMS_OUTPUT.PUT_LINE('FALSE');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE('TRUE');
     END IF;
+    CLOSE c_actividad_abonada;
 END;
 /
 
+
+---Funciona correctamente
+EXEC ActividadAbonada ('06852683V','A302'); ---true
+EXEC ActividadAbonada ('69191424H','B302'); ---false
+
+---Fallo
+EXEC ActividadAbonada ('54890865P','A002');
 
 ---Procedimiento ComprobarPago que muestrer TRUE si el cliente ha pagado la última actividad con ese código que ha realizado y un FALSE en caso contrario.
 CREATE OR REPLACE PROCEDURE ComprobarPago (v_codcliente personas.NIF%type, v_codactividad actividades.codigo%type) IS
@@ -113,7 +126,12 @@ BEGIN
 END;
 /
     
+
+EXEC ComprobarPago  ('06852683V','A032'); ---true
     
-    
-    
-    
+EXEC ComprobarPago  ('69191424H','B302'); ---false
+
+EXEC ComprobarPago ('54890869P','A999'); ---false
+
+
+---PROCEDIMIENTO FINALIZADO
