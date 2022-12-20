7dc6aa58080ae7caef839ed10ec16921bff7caa7
alepeteporico <tojandro@gmail.com>
2022-12-20 18:00:18 +0100

    primer intento de rellenar la columna balance hotel

--- Procedimiento5.sql
+++ Procedimiento5.sql
@@ -26,10 +26,23 @@ END;
 
 CREATE OR REPLACE PROCEDURE RellenarBalance(v_codactividad actividadesrealizadas.codigoactividad%rowtype, v_codestancia actividadesrealizadas.codigoestancia%rowtype, v_fecha actividadesrealizadas.fecha%rowtype)
 IS
+    CURSOR c_actividades IS
+    SELECT CodigoActividad, CodigoEstancia, Fecha
+    FROM ActividadesRealizadas;
 BEGIN
+    OPEN c_actividades;
+    FETCH c_actividades INTO v_codactividad, v_codestancia, v_fecha;
+
+    WHILE c_actividades%FOUND LOOP
     UPDATE ActividadesRealizadas
     SET BalanceHotel = NVL(SELECT PrecioporPersona, ComisionHotel, CostePersonaparaHotel
                             FROM Actividades
-                            WHERE Codigo=) * (SELECT NumPersonas
+                            WHERE Codigo=v_codactividad) * (SELECT NumPersonas
                                                 FROM ActividadesRealizadas
-                                                WHERE )
+                                                WHERE CodigoActividad = v_codactividad
+                                                AND CodigoEstancia = v_codestancia
+                                                AND Fecha = v_fecha);
+    END LOOP;
+    CLOSE c_actividades;
+END;
+/
